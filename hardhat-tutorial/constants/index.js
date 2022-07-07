00dd0a0b75123427ee76e085156ef0507730a782
MSBivens <mikebiv95@gmail.com>
2022-07-06 21:03:40 -0500

    updated with vercel link

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x0f7B9965Cbe9dde6d9FDE9a8E79dE6C2D54CA112";
 // URL for the Crypto Devs NFT Metadata
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL =
+  "https://vercel.com/msbivens/learn-web3-dao-nft-collection/D64Htj74HFmxuJwGDsxYCbcXQMQY/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
